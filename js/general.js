8014d71c8e3820a12eebd9a15db39ddd0918d2a1
Turalinov <rusltur_24@mail.ru>
2023-05-26 01:25:01 +0600

    Updates

---
+++ js/general.js
@@ -0,0 +1,165 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/general.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/blocks/solutions/solutions.js":
+/*!*******************************************!*\
+  !*** ./src/blocks/solutions/solutions.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return solutions; });
+function solutions() {
+  console.log('solutions');
+  var variants = document.querySelectorAll('.variant__item');
+  if (variants) {
+    var deleteActiveClassVariants = function deleteActiveClassVariants() {
+      variants.forEach(function (variant) {
+        variant.classList.remove('active');
+      });
+    };
+    var setActiveClassVariant = function setActiveClassVariant(id) {
+      variants[id].classList.add('active');
+    };
+    var deleteActiveClassChoises = function deleteActiveClassChoises() {
+      choises.forEach(function (choise) {
+        choise.classList.remove('active');
+      });
+    };
+    var setActiveClassChoise = function setActiveClassChoise(id) {
+      choises[id].classList.add('active');
+    };
+    var choises = document.querySelectorAll('.choise__item');
+    variants.forEach(function (variant, index) {
+      var link = variant.querySelector('.variant__link');
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        deleteActiveClassVariants();
+        setActiveClassVariant(index);
+        deleteActiveClassChoises();
+        setActiveClassChoise(index);
+      });
+    });
+  }
+}
+
+/***/ }),
+
+/***/ "./src/js/general.js":
+/*!***************************!*\
+  !*** ./src/js/general.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_solutions_solutions__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! %modules%/solutions/solutions */ "./src/blocks/solutions/solutions.js");
+//preloader
+// import preloader from '%modules%/preloader/preloader'
+// //toggle
+// import button from '%modules%/toggle/toggle'
+// //menu
+// import menu from '%modules%/menu/menu'
+// //form
+// import form from '%modules%/form/form'//
+//solutions
+
+
+// preloader();
+// button();
+// menu();
+// form();
+Object(_modules_solutions_solutions__WEBPACK_IMPORTED_MODULE_0__["default"])();
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=general.js.map
